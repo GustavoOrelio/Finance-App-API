278b57a27ca8958e79be9b4b2fb6e6f00a7e9449
Gustavo Orelio <oreliogustavo@gmail.com>
2025-04-26 15:10:29 -0300

    test: ensure DeleteUserController returns 404 if UserNotFoundError is thrown

--- src/controllers/user/delete-user.test.js
+++ src/controllers/user/delete-user.test.js
@@ -1,55 +1,86 @@
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
-import { prisma } from '../../../../prisma/prisma'
-import { user } from '../../../tests'
-import { PostgresDeleteUserRepository } from './delete-user'
-import { UserNotFoundError } from '../../../errors'
+import { faker } from '@faker-js/faker'
+import { DeleteUserController } from './delete-user'
+import { user } from '../../tests'
+import { UserNotFoundError } from '../../errors'
 
-describe('PostgresDeleteUserRepository', () => {
-    it('should delete a user on db', async () => {
-        await prisma.user.create({
-            data: user,
-        })
+describe('DeleteUserController', () => {
+    class DeleteUserUseCaseStub {
+        async execute() {
+            return user
+        }
+    }
+    const makeSut = () => {
+        const deleteUserUseCase = new DeleteUserUseCaseStub()
+        const sut = new DeleteUserController(deleteUserUseCase)
 
-        const sut = new PostgresDeleteUserRepository()
+        return { deleteUserUseCase, sut }
+    }
 
-        const result = await sut.execute(user.id)
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+    }
 
-        expect(result).toStrictEqual(user)
+    it('should return 200 if user is deleted', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(200)
     })
 
-    it('should call Prisma with correct params', async () => {
-        await prisma.user.create({ data: user })
-        const sut = new PostgresDeleteUserRepository()
-        const prismaSpy = jest.spyOn(prisma.user, 'delete')
+    it('should return 400 if id is invalid', async () => {
+        // arrange
+        const { sut } = makeSut()
 
-        await sut.execute(user.id)
+        // act
+        const result = await sut.execute({ params: { userId: 'invalid_id' } })
 
-        expect(prismaSpy).toHaveBeenCalledWith({
-            where: {
-                id: user.id,
-            },
-        })
+        // assert
+        expect(result.statusCode).toBe(400)
     })
 
-    it('should throw generic error if Prisma throws generic error', async () => {
-        const sut = new PostgresDeleteUserRepository()
-        jest.spyOn(prisma.user, 'delete').mockRejectedValueOnce(new Error())
-
-        const promise = sut.execute(user.id)
-
-        await expect(promise).rejects.toThrow()
-    })
-
-    it('should throw generic error if Prisma throws generic error', async () => {
-        const sut = new PostgresDeleteUserRepository()
-        jest.spyOn(prisma.user, 'delete').mockRejectedValueOnce(
-            new PrismaClientKnownRequestError('', {
-                code: 'P2025',
-            }),
+    it('should return 404 if user is not found', async () => {
+        // arrange
+        const { sut, deleteUserUseCase } = makeSut()
+        jest.spyOn(deleteUserUseCase, 'execute').mockRejectedValueOnce(
+            new UserNotFoundError(),
         )
 
-        const promise = sut.execute(user.id)
+        // act
+        const result = await sut.execute(httpRequest)
 
-        await expect(promise).rejects.toThrow(new UserNotFoundError(user.id))
+        // assert
+        expect(result.statusCode).toBe(404)
+    })
+
+    it('should return 500 if DeleteUserUseCase throws', async () => {
+        // arrange
+        const { sut, deleteUserUseCase } = makeSut()
+        jest.spyOn(deleteUserUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        // act
+        const result = await sut.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(500)
+    })
+
+    it('should call DeleteUserUseCase with correct params', async () => {
+        // arrange
+        const { sut, deleteUserUseCase } = makeSut()
+        const executeSpy = jest.spyOn(deleteUserUseCase, 'execute')
+
+        // act
+        await sut.execute(httpRequest)
+
+        // assert
+        expect(executeSpy).toHaveBeenCalledWith(httpRequest.params.userId)
     })
 })
